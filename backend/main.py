fe89f3d2d19c5c30b6f41604b6602f070479a600
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-04-06 22:12:20 +0530

    Api routes added

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# Get list of jobs route 
+@app.get("/jobs")
+def get_jobs():
+    return {"jobs": []}
+
+
+# Status updating route
+@app.get("/status")
+def update_status():
+    return {"status": "updated"}
+
+
+# Trigger for scraper
+@app.get("/run-scrapper")
+def run_scrapper():
+    return {"message": "Scrapper triggered"}
+
